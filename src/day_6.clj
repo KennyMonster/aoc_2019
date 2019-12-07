53de3f3a83ec6ebfd4a62af9078f7c1d3d211233
Ken Johnson <risinglight@gmail.com>
2019-12-07 14:11:14 -0800

    Parsing progress

--- src/day_6.clj
+++ src/day_6.clj
@@ -1,8 +1,20 @@
-(ns day-6)
+(ns day-6
+  (:require [clojure.string :as str]))
 
 
-(defn ret6 [] 6)
+(def input-filename "input/day_6_input")
 
+(defn parse-input [s]
+  (->> s
+       (str/split-lines)
+       (map #(str/split % #"\)"))
+       (map reverse)
+       (map vec)
+       (into {})))
 
+;(parse-input (slurp input-filename))
+
+(def test-input
+  "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L")
 
 
--- test/day_6_test.clj
+++ test/day_6_test.clj
@@ -1,5 +1,6 @@
 (ns day-6-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [day-6 :refer :all]))
 
 
 
@@ -7,5 +8,6 @@
 ;(deftest test-ret6
 ;  (is (= 6 (ret6))))
 
-(deftest stupid
-  (is (= 1 2)))
+(deftest test-parse-input
+  (is (= (parse-input test-input)
+         {"K" "J", "L" "K", "G" "B", "J" "E", "H" "G", "E" "D", "C" "B", "F" "E", "B" "COM", "I" "D", "D" "C"})))
